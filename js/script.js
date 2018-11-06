cfbc248214cf20feccba3db2e1bb808d802fa08b
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2018-11-06 23:24:16 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+/* code Google anal à coller dans JS */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-113379848-1', 'auto');
+ga('send', 'pageview');
+
+ 
+  
+        
+        $(function() {
+        
+            window.sr = ScrollReveal({
+                distance: '1000px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+        
+        })
+
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-113379848-1');
