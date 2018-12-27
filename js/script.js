569c6a8d12d5ea252758106f1411a469acc290e9
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2018-12-27 18:03:06 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -9,23 +9,20 @@ ga('create', 'UA-113379848-1', 'auto');
 ga('send', 'pageview');
 
  
-  /*
+/*
 animation widget from here: https://github.com/silexlabs/Silex/issues/443
 */
-        
-        $(function() {
-        
-            window.sr = ScrollReveal({
-                distance: '1000px'
-            });
-            sr.reveal('.from-left', { origin:  'left'});
-            sr.reveal('.from-right', { origin:  'right'});
-            sr.reveal('.from-top', { origin:  'top'});
-            sr.reveal('.from-bottom', { origin:  'bottom'});
-        
-        })
+$(function() {
 
+    window.sr = ScrollReveal({
+        distance: '1000px'
+    });
+    sr.reveal('.from-left', { origin:  'left'});
+    sr.reveal('.from-right', { origin:  'right'});
+    sr.reveal('.from-top', { origin:  'top'});
+    sr.reveal('.from-bottom', { origin:  'bottom'});
 
+})
 
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
