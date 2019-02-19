58aa6d5ae5192d4b5a03ba1c2fef6b885cacb2fa
BaguetteAgile <35597855+BaguetteAgile@users.noreply.github.com>
2019-02-19 09:37:16 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,31 +1,22 @@
 
-/* code Google anal à coller dans JS */
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-113379848-1', 'auto');
-ga('send', 'pageview');
-
- 
-/*
-animation widget from here: https://github.com/silexlabs/Silex/issues/443
-*/
-$(function() {
-
-    window.sr = ScrollReveal({
-        distance: '1000px'
-    });
-    sr.reveal('.from-left', { origin:  'left'});
-    sr.reveal('.from-right', { origin:  'right'});
-    sr.reveal('.from-top', { origin:  'top'});
-    sr.reveal('.from-bottom', { origin:  'bottom'});
-
-})
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-113379848-1');
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
+            });
+        
+            /*
+                animation widget from here: https://github.com/silexlabs/Silex/issues/443
+            */
+        
+            window.sr = ScrollReveal({
+                distance: '100px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+        
+        })
+    
